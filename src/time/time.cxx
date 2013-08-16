bd881eb30f3e777d8bb0281c88b98bc57af8e056
Devin Matthews <dmatthews@utexas.edu>
2013-08-16 12:25:48 -0500

    Added timers for OSX

--- src/time/time.cxx
+++ src/time/time.cxx
@@ -27,6 +27,11 @@
 #include <string>
 #include <cstdio>
 #include <cassert>
+#include <algorithm>
+
+#ifdef _OPENMP
+#include "omp.h"
+#endif
 
 #include "util/util.h"
 
@@ -34,110 +39,148 @@
 
 using namespace std;
 
+bool operator<(const timespec& t1, const timespec& t2)
+{
+    return t1.tv_sec < t2.tv_sec || (!(t2.tv_sec < t1.tv_sec) && t1.tv_nsec < t2.tv_nsec);
+}
+
+timespec& operator+=(timespec& t1, const timespec& t2)
+{
+    t1.tv_sec += t2.tv_sec;
+    t1.tv_nsec += t2.tv_nsec;
+    if (t1.tv_nsec >= 1000000000)
+    {
+        t1.tv_nsec -= 1000000000;
+        t1.tv_sec++;
+    }
+    return t1;
+}
+
+timespec& operator-=(timespec& t1, const timespec& t2)
+{
+    if (t1.tv_sec < t2.tv_sec || (t1.tv_sec == t2.tv_sec && t1.tv_nsec < t2.tv_nsec))
+    {
+        printf("%ld %ld %ld %ld\n", t1.tv_sec, t1.tv_nsec, t2.tv_sec, t2.tv_nsec);
+        assert(0);
+        t1.tv_sec = 0;
+        t1.tv_nsec = 0;
+        return t1;
+    }
+
+    t1.tv_sec -= t2.tv_sec;
+    if (t1.tv_nsec < t2.tv_nsec)
+    {
+        t1.tv_nsec += 1000000000;
+        t1.tv_sec--;
+    }
+    t1.tv_nsec -= t2.tv_nsec;
+    return t1;
+}
+
+timespec& operator*=(timespec& t, const int m)
+{
+    t.tv_sec *= m;
+    t.tv_nsec *= m;
+    t.tv_sec += t.tv_nsec/1000000000;
+    t.tv_nsec = t.tv_nsec%1000000000;
+    return t;
+}
+
+timespec& operator/=(timespec& t, const int m)
+{
+    long tmp = t.tv_sec%m;
+    t.tv_sec /= m;
+    t.tv_nsec = (t.tv_nsec+tmp*1000000000)/m;
+    t.tv_sec += t.tv_nsec/1000000000;
+    t.tv_nsec = t.tv_nsec%1000000000;
+    return t;
+}
+
+timespec operator+(const timespec& t1, const timespec& t2)
+{
+    timespec r = t1;
+    r += t2;
+    return r;
+}
+
+timespec operator-(const timespec& t1, const timespec& t2)
+{
+    timespec r = t1;
+    r -= t2;
+    return r;
+}
+
+timespec operator*(const timespec& t, const int m)
+{
+    timespec r = t;
+    r *= m;
+    return r;
+}
+
+timespec operator/(const timespec& t, const int m)
+{
+    timespec r = t;
+    r /= m;
+    return r;
+}
+
 namespace aquarius
 {
 namespace time
 {
 
-static timespec delta = {0,-1};
-static vector< pair<timespec,timespec> > tics;
+//static timespec delta = {0,-1};
+static vector< pair<timespec,timespec> > *tics[128];
 static vector< pair<string, pair<timespec,long> > > timers;
-
-timespec& operator+=(timespec& t1, const timespec& t2)
-{
-	t1.tv_sec += t2.tv_sec;
-	t1.tv_nsec += t2.tv_nsec;
-	if (t1.tv_nsec >= 1000000000)
-	{
-		t1.tv_nsec -= 1000000000;
-		t1.tv_sec++;
-	}
-	return t1;
-}
-
-timespec& operator-=(timespec& t1, const timespec& t2)
-{
-	if (t1.tv_sec < t2.tv_sec || (t1.tv_sec == t2.tv_sec && t1.tv_nsec < t2.tv_nsec))
-	{
-		printf("%ld %ld %ld %ld\n", t1.tv_sec, t1.tv_nsec, t2.tv_sec, t2.tv_nsec);
-		assert(0);
-		t1.tv_sec = 0;
-		t1.tv_nsec = 0;
-		return t1;
-	}
-
-	t1.tv_sec -= t2.tv_sec;
-	if (t1.tv_nsec < t2.tv_nsec)
-	{
-		t1.tv_nsec += 1000000000;
-		t1.tv_sec--;
-	}
-	t1.tv_nsec -= t2.tv_nsec;
-	return t1;
-}
-
-timespec& operator*=(timespec& t, const int m)
-{
-	t.tv_sec *= m;
-	t.tv_nsec *= m;
-	t.tv_sec += t.tv_nsec/1000000000;
-	t.tv_nsec = t.tv_nsec%1000000000;
-	return t;
-}
-
-timespec& operator/=(timespec& t, const int m)
-{
-	long tmp = t.tv_sec%m;
-	t.tv_sec /= m;
-	t.tv_nsec = (t.tv_nsec+tmp*1000000000)/m;
-	t.tv_sec += t.tv_nsec/1000000000;
-	t.tv_nsec = t.tv_nsec%1000000000;
-	return t;
-}
-
-timespec operator+(const timespec& t1, const timespec& t2)
-{
-	timespec r = t1;
-	r += t2;
-	return r;
-}
-
-timespec operator-(const timespec& t1, const timespec& t2)
-{
-	timespec r = t1;
-	r -= t2;
-	return r;
-}
-
-timespec operator*(const timespec& t, const int m)
-{
-	timespec r = t;
-	r *= m;
-	return r;
-}
-
-timespec operator/(const timespec& t, const int m)
-{
-	timespec r = t;
-	r /= m;
-	return r;
-}
 
 double todouble(const timespec& t)
 {
-	return double(t.tv_sec)+double(t.tv_nsec)/1e9;
+#ifdef __MACH__
+    static double conv = -1.0;
+    if (conv < 0)
+    {
+        mach_timebase_info_data_t timebase;
+        mach_timebase_info(&timebase);
+        conv = (double)timebase.numer / (double)timebase.denom;
+    }
+    return conv*(double(t.tv_sec)+double(t.tv_nsec)/1e9);
+#else
+    return double(t.tv_sec)+double(t.tv_nsec)/1e9;
+#endif
 }
 
 int register_timer(const string& name)
 {
-    timers.push_back(make_pair(name, make_pair(timespec(), 0l)));
-    return timers.size()-1;
+    int timer = 0;
+    #pragma omp critical
+    {
+        int i;
+        for (i = 0;i < timers.size();i++)
+        {
+            if (timers[i].first == name) break;
+        }
+        timer = i;
+        if (timer == timers.size()) timers.push_back(make_pair(name, make_pair((timespec){0,0}, 0l)));
+    }
+    return timer;
 }
 
 void inc_timer(const int timer, const timespec& time)
 {
-    timers[timer].second.first += time;
-    timers[timer].second.second++;
+    #ifdef _OPENMP
+    if (!omp_in_parallel())
+    {
+        int nt = omp_get_max_threads();
+        timers[timer].second.first += time*nt;
+        timers[timer].second.second++;
+    }
+    else
+    #endif
+    #pragma omp critical
+    {
+        timers[timer].second.first += time;
+        timers[timer].second.second++;
+    }
 }
 
 void print_timers()
@@ -148,20 +191,38 @@ void print_timers()
         max_len = max(max_len, (int)it->first.size());
     }
 
+    sort(timers.begin(), timers.end());
+
     for (vector< pair<string, pair<timespec,long> > >::iterator it = timers.begin();it != timers.end();++it)
     {
         double tot = allsum(todouble(it->second.first));
         long count = allsum(it->second.second);
-        PRINT("%s:%*s %13.6fs %10ld\n", it->first.c_str(), (int)(max_len-it->first.size()), "", tot, count);
+        PRINT("%s:%*s %13.6f s %10ld x\n", it->first.c_str(), (int)(max_len-it->first.size()), "", tot, count);
+    }
+}
+
+void clear_timers()
+{
+    for (vector< pair<string, pair<timespec,long> > >::iterator it = timers.begin();it != timers.end();++it)
+    {
+        it->second.first.tv_sec = 0;
+        it->second.first.tv_nsec = 0;
+        it->second.second = 0;
     }
 }
 
 void tic()
 {
-	/*
+    #ifdef _OPENMP
+    int tid = omp_get_thread_num();
+    #else
+    int tid = 0;
+    #endif
+
+    /*
     if (delta.tv_nsec == -1)
     {
-    	delta.tv_nsec = 0;
+        delta.tv_nsec = 0;
 
         timespec dt = {0,0};
         for (int i = 0;i < 10;i++)
@@ -176,16 +237,43 @@ void tic()
     }
     */
 
-    tics.push_back(make_pair(timespec(),timespec()));
-    clock_gettime(CLOCK_REALTIME, &(tics.back().first));
+    if (tics[tid] == NULL) tics[tid] = new vector< pair<timespec,timespec> >();
+
+    //PRINT("tic\n");
+    tics[tid]->push_back(make_pair(timespec(),timespec()));
+#ifdef __MACH__
+    uint64_t nsec = mach_absolute_time();
+    tics[tid]->back().first.tv_sec = nsec/1000000000;
+    tics[tid]->back().first.tv_nsec = nsec%1000000000;
+#else
+    int ret = clock_gettime(CLOCK_MONOTONIC, &(tics[tid]->back().first));
+    if (ret != 0) ERROR("clock_gettime");
+#endif
+    //ALLPRINT("tic: %ld %ld\n", tics[tid]->back().first.tv_sec, tics[tid]->back().first.tv_nsec);
 }
 
 timespec toc()
 {
-    clock_gettime(CLOCK_REALTIME, &(tics.back().second));
-    //timespec dt = (tics.back().second-tics.back().first)-delta;
-    timespec dt = tics.back().second-tics.back().first;
-    tics.pop_back();
+    #ifdef _OPENMP
+    int tid = omp_get_thread_num();
+    #else
+    int tid = 0;
+    #endif
+
+    //PRINT("toc\n");
+    timespec dt = (timespec){0,0};
+#ifdef __MACH__
+    uint64_t nsec = mach_absolute_time();
+    tics[tid]->back().second.tv_sec = nsec/1000000000;
+    tics[tid]->back().second.tv_nsec = nsec%1000000000;
+#else
+    int ret = clock_gettime(CLOCK_MONOTONIC, &(tics[tid]->back().second));
+    if (ret != 0) ERROR("clock_gettime");
+#endif
+    //ALLPRINT("toc: %ld %ld\n", tics[tid]->back().second.tv_sec, tics[tid]->back().second.tv_nsec);
+    //timespec dt = (tics->back().second-tics[tid]->back().first)-delta;
+    dt = tics[tid]->back().second-tics[tid]->back().first;
+    tics[tid]->pop_back();
     return dt;
 }
 
